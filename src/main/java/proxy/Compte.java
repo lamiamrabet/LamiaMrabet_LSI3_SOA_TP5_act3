27aed2861034ddab412e017b1d39508b38d23fd6
Lamia Mrabet <lamiamrabet456@gmail.com>
2023-11-12 22:11:59 +0100

    init

---
+++ src/main/java/proxy/Compte.java
@@ -0,0 +1,79 @@
+
+package proxy;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour compte complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>{@code
+ * <complexType name="compte">
+ *   <complexContent>
+ *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       <sequence>
+ *         <element name="code" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         <element name="solde" type="{http://www.w3.org/2001/XMLSchema}double" minOccurs="0"/>
+ *       </sequence>
+ *     </restriction>
+ *   </complexContent>
+ * </complexType>
+ * }</pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "compte", propOrder = {
+    "code",
+    "solde"
+})
+public class Compte {
+
+    protected int code;
+    protected Double solde;
+
+    /**
+     * Obtient la valeur de la propriété code.
+     * 
+     */
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * Définit la valeur de la propriété code.
+     * 
+     */
+    public void setCode(int value) {
+        this.code = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété solde.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Double }
+     *     
+     */
+    public Double getSolde() {
+        return solde;
+    }
+
+    /**
+     * Définit la valeur de la propriété solde.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Double }
+     *     
+     */
+    public void setSolde(Double value) {
+        this.solde = value;
+    }
+
+}
